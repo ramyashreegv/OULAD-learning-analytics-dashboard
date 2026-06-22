1d65874393cc419083196f8df45141b88c65cbcb
Ramyashree GV <ramyashree.gv@gmail.com>
2026-06-22 16:09:33 +0530

    Refactor file path and clean up code

--- scripts/06_studentVle_quality.py
+++ scripts/06_studentVle_quality.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\CR SHARMA\Desktop\My Courses\Data Analytics\Projects\P1 - EdTech\OULAD Datasets\studentVle.csv")
+df = pd.read_csv(r"OULAD Datasets\studentVle.csv")
 
 def basic_check(df, name):
     print(f"\n===== {name} =====")
